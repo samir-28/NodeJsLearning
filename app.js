d78f05623a0177f93b51a3629bc54674fce974ab
Sawmeer Bazgain <samirbajgain9@gmail.com>
2024-08-20 20:08:09 +0545

    Updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set('view engine','ejs')
-
+require("./model/index")
 
 app.get("/", (req, res) => {
   const data={
--- model/index.js
+++ model/index.js
@@ -1,1 +1,36 @@
-const {Sequelize,DataTypes}=require('sequelize')
+const {Sequelize,DataTypes, Error}=require('sequelize')
+const sequelize=new Sequelize('haha','root','',{
+    host:'localhost',
+    port:3306,
+    dialect:'mysql',
+    operatorsAliases: false,  
+    pool:{               //waiting time for database connection
+        max:5,             
+        min:0,
+        acquire:3000,
+        idle:10000
+    }
+})
+
+sequelize.authenticate()
+.then(()=>{
+    console.log("Correct username and password")
+})
+.catch((err)=>{
+    console.log("There is Error",err)
+})
+
+const db={}
+db.Sequelize=Sequelize
+db.sequelize=sequelize
+
+//db={
+ //   Sequelize:Sequelize,
+ //   sequelize:sequelize
+//}
+
+db.sequelize.sync({force:false}).then(()=>{
+    console.log("Synced done")
+})
+
+module.exports=db
